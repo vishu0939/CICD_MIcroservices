1edda525ad21bf1374649999fdc6a82e2796309b
root <root@ip-172-31-29-140.eu-west-1.compute.internal>
2026-07-14 06:56:34 +0000

    Cicd-microservices

---
+++ microservices-project/product-service/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def products():
+    return {
+        "service": "Product Service",
+        "message": "Product details retrieved successfully!"
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5002)
+
